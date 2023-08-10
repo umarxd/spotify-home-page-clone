80f75955d24b6165364ca899be6cf061ff133d16
umarxd <xqcowsavedmylife@gmail.com>
2023-08-10 19:27:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="text-red-400">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos nesciunt
+        voluptatem perspiciatis sint fuga dignissimos eligendi numquam alias
+        beatae laboriosam exercitationem excepturi quasi quibusdam, iste
+        corporis cum ratione in. Saepe.
+      </div>
+    </>
+  );
+}
+
+export default App;
