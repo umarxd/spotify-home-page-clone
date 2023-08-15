9d715adb35707f49b204c83ffec47338ce1da585
umarxd <xqcowsavedmylife@gmail.com>
2023-08-15 23:18:08 +0300

    add basic stylings for music player component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import MainNav from "./components/MainNav";
+import MusicPlayer from "./components/MusicPlayer";
 import Playlists from "./components/Playlists";
 
 function App() {
   return (
     <>
-      <div className="text-white w-96 p-2 flex flex-col h-screen">
+      <div className="text-white w-96 p-2 flex flex-col h-[90vh]">
         <MainNav />
         <Playlists />
       </div>
+      <MusicPlayer />
     </>
   );
 }
---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,9 @@
+function MusicPlayer() {
+  return (
+    <div className="bg-black text-white w-full h-24 absolute bottom-0">
+      MusicPlayer
+    </div>
+  );
+}
+
+export default MusicPlayer;
